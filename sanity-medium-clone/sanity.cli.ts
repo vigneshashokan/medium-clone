88c691a13d7bd4f0e65bb0eb270658d3a8dc414e
Vignesh Ashokan <vigneshashokan@gmail.com>
2023-10-09 12:26:07 -0700

    .vercelignore

--- sanity-medium-clone/sanity.cli.ts
+++ sanity-medium-clone/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'wp3rex57',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
